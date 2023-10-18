71abcdbfea212b13d4023d87360f1847a91575e6
Guilherme Vanzin Hoffmann <guilhermevanzin@gmail.com>
2023-10-18 12:15:59 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/guilhermehoffmann/appcep/cepapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.guilhermehoffmann.appcep.cepapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:cepapp/my_app.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+Future main() async {
+  await dotenv.load(fileName: ".env");
+  runApp(const MyApp());
+}
